4335f597efe9a03aa7a054b3de32d24eedd3b476
Sergius92739 <sergem279@gmail.com>
2021-08-03 00:31:31 +0300

    added a function with recursion

--- tasks/intro/intro.js
+++ tasks/intro/intro.js
@@ -2,22 +2,34 @@
 // Данный код возвращает сумму разностей предыдущего эл-та из следующего, переданного массива. Асимптотика: время O(n), память O(1) 
 
 // Задача 2
-const arr = [14, 16, 19, 32, 32, 32, 56, 69, 72];
 
-function getNumOfLargeSizedBooks(arr, bookSize) {
+const arr = [14, 16, 19, 32, 32, 32, 56, 69, 72];
+// Вариант с циклом
+function getNum1(arr, bookSize) {
   let left = 0;
   let right = arr.length - 1;
-
+ 
   while (left <= right) {
    const middle = Math.floor((left + right) / 2);
     if (arr[middle] <= bookSize && arr[middle + 1] > bookSize) {
       return arr.length - (middle + 1);
-    } else if (arr[middle] > bookSize) {
-      right = middle - 1;
     } else {
-      left = middle + 1;
-    }
+     arr[middle] > bookSize ? right = middle - 1 : left = middle + 1
+    } 
   }
 }
+console.log(getNum1(arr, 32))
 
-console.log(getNumOfLargeSizedBooks(arr, 32))
+// Вариант с рекурсией
+function getNum2(left, right, arr, bookSize) {
+  const middle = Math.floor((left + right) / 2);
+
+  if (arr[middle] <= bookSize && arr[middle + 1] > bookSize) {
+    return arr.length - (middle + 1);
+  }
+  
+  return arr[middle] > bookSize
+   ? getNum2(left, middle - 1, arr, bookSize)
+   : getNum2(middle + 1, right, arr, bookSize);
+}
+console.log(getNum2(0, arr.length - 1, arr, 32));
